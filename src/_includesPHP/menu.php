f178058d7256a142630bae3df48cb289eddf902f
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-10-09 01:34:18 +0300

    update menu

--- src/_includesPHP/menu.php
+++ src/_includesPHP/menu.php
@@ -1,7 +1,23 @@
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/add/">Add element to table</a></li>
-        <li><a href="/show/">Show table</a></li>
-    </ul>
+<style>
+    body
+    {
+        padding-top: 4.5rem;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="https://github.com/PavelGalanin2001">@PavelGalanin2001 site</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/add/">Add</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/show/">Show</a>
+            </li>
+        </ul>
+    </div>
 </nav>
