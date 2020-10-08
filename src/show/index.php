1e22668decc6a20297d6f682306a7d32cf2e0c10
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-10-09 01:40:28 +0300

    update table

--- src/show/index.php
+++ src/show/index.php
@@ -5,32 +5,39 @@
     include '../_includesPHP/connect.php';
 ?>
 
-<table border="1">
-    <caption>Table. Show</caption>
-    <tr>
-        <th>id</th>
-        <th>Name</th>
-        <th>Model</th>
-        <th>On box</th>
-        <th>Cost</th>
-    </tr>
-    <?php
-        // = = = = = - - - - - = = = = = get arr from table
-        $sql = "SELECT * FROM $table";
-        $result = $connect->query($sql);
+<div class="container">
+    <table class="table">
+        <caption>Table. Show</caption>
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Model</th>
+                <th scope="col">On box</th>
+                <th scope="col">Cost</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                // = = = = = - - - - - = = = = = get arr from table
+                $sql = "SELECT * FROM $table";
+                $result = $connect->query($sql);
 
-        for($item = mysqli_fetch_assoc($result); $item != ""; $item = mysqli_fetch_assoc($result))
-        {
-            echo "<tr>"
-                    . "<td>" . $item['id'] . "</td>"
-                    . "<td>" . $item['model'] . "</td>"
-                    . "<td>" . "-" . "</td>"
-                    . "<td>" . "0" . "</td>"
-                    . "<td>" . "00.00" . "</td>"
-                . "</tr>";
-        }
+                for($item = mysqli_fetch_assoc($result); $item != ""; $item = mysqli_fetch_assoc($result))
+                {
+                    echo "<tr>"
+                            . "<td scope=\"row\">" . $item['id'] . "</td>"
+                            . "<td>" . $item['model'] . "</td>"
+                            . "<td>" . "-" . "</td>"
+                            . "<td>" . "0" . "</td>"
+                            . "<td>" . "00.00" . "</td>"
+                        . "</tr>";
+                }
 
-        // = = = = = - - - - - = = = = = close connect
-        $connect->close();
-    ?>
-</table>
+                // = = = = = - - - - - = = = = = close connect
+                $connect->close();
+            ?>
+        </tbody>
+    </table>
+</div>
+
