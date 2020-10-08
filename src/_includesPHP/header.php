195c7fad049298c4a67d61405c8e335188c22f8a
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-10-09 01:54:43 +0300

    update form

--- src/_includesPHP/header.php
+++ src/_includesPHP/header.php
@@ -7,5 +7,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $title; ?> - course work YP (3 semestr)</title>
     <link rel="stylesheet" href="/_libs/bootstrap-4.5.2-dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/_CSS/form.css" />
 </head>
--- src/add/index.php
+++ src/add/index.php
@@ -2,48 +2,69 @@
     $title = "Add element page";
     include '../_includesPHP/header.php';
     include '../_includesPHP/menu.php';
-
-    // = = = = = - - - - - = = = = = global vars
-    $model = "222";
-
-    include "../_includesPHP/connect.php";
-
-    // add element to table
-    $sql = "INSERT $table (model)
-            VALUES ('$model')";
-    if ($connect->query($sql) == TRUE)
-    {
-        echo "<br />" . "Element are add to table" . "<br />";
-    }
-    else
-    {
-        echo "<br />" . "Element aren't add to table" . "<br />";
-    }
-
-    // = = = = = - - - - - = = = = = close connect
-    $connect->close();
 ?>
 
-<form action="/php/form.php" method="post" class="form">
-    <input
-        type="text"
-        placeholder="Наименование"
-        name="name"
-    />
-    <input
-        type="text"
-        placeholder="Модель"
-        name="model"
-    />
-    <input
-        type="text"
-        placeholder="Количество в коробке"
-        name="onBox"
-    />
-    <input
-        type="text"
-        placeholder="Цена без НДС"
-        name="cost"
-    />
-    <button type="submit" name>Отправить</button>
-</form>
+<div class="container">
+    <form action="/php/form.php" method="post">
+        <div class="form-group">
+            <label>Name</label>
+            <input
+                class="form-control"
+                type="text"
+                name="name"
+            />
+            <small class="form-text text-muted">For example: 2 Gand Earthed Socket</small>
+        </div>
+        <div class="form-group">
+            <label>Model</label>
+            <input
+                class="form-control"
+                type="text"
+                name="model"
+            />
+            <small class="form-text text-muted">For example: 920100</small>
+        </div>
+        <div class="form-group">
+            <label>On box</label>
+            <input
+                class="form-control"
+                type="text"
+                name="onBox"
+            />
+            <small class="form-text text-muted">For example: 24</small>
+        </div>
+        <div class="form-group">
+            <label>Cost</label>
+            <input
+                class="form-control"
+                type="text"
+                name="cost"
+            />
+            <small class="form-text text-muted">For example: 00.00</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <?php
+        // = = = = = - - - - - = = = = = global vars
+        $model = "222";
+
+        include "../_includesPHP/connect.php";
+
+        // add element to table
+        $sql = "INSERT $table (model)
+                VALUES ('$model')";
+        if ($connect->query($sql) == TRUE)
+        {
+            echo "<br />" . "Element are add to table" . "<br />";
+        }
+        else
+        {
+            echo "<br />" . "Element aren't add to table" . "<br />";
+        }
+
+        // = = = = = - - - - - = = = = = close connect
+        $connect->close();
+    ?>
+</div>
+
