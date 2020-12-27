b1eb10a67094a28a1b375d31ae11af65ad3abe25
Pavel-Innokentevich-Galanin <PavelGalanin2001@outlook.com>
2020-12-28 00:41:38 +0300

    update menu

--- src/_includesHTML/menu.php
+++ src/_includesHTML/menu.php
@@ -6,23 +6,41 @@
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <a class="navbar-brand" href="/">Admin Panel</a>
+    <a
+        class="navbar-brand"
+        href="/"
+    >Admin Panel</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
+                <a
+                    class="nav-link text-danger"
+                    href="/delete-table/"
+                >Delete table</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/create-table/">Create table</a>
+                <a
+                    class="nav-link text-success"
+                    href="/create-table/"
+                >Create table</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/delete-table/">Delete table</a>
+                <a
+                    class="nav-link"
+                    href="/"
+                >Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/add/">Add</a>
+                <a
+                    class="nav-link"
+                    href="/add/"
+                >Add</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/show/">Show</a>
+                <a
+                    class="nav-link"
+                    href="/show/"
+                >Show</a>
             </li>
         </ul>
     </div>
