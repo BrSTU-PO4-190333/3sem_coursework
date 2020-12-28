16a94341346799eade7eb37aa42ab31d41205aeb
Pavel-Innokentevich-Galanin <PavelGalanin2001@outlook.com>
2020-12-28 03:25:03 +0300

    HTML form in other file

---
+++ src/_includesHTML/formInner.php
@@ -0,0 +1,61 @@
+<div class="form-group">
+    <label>Model</label>
+    <input
+        class="form-control"
+        type="text"
+        name="model"
+        value="<?= $model ?>"
+    />
+    <small class="form-text text-muted">For example: 920100</small>
+</div>
+<div class="form-group">
+    <label>Name</label>
+    <input
+        class="form-control"
+        type="text"
+        name="name"
+        value="<?= $name ?>"
+    />
+    <small class="form-text text-muted">For example: 2 Gand Earthed Socket</small>
+</div>
+<div class="form-group">
+    <label>On box</label>
+    <input
+        class="form-control"
+        type="text"
+        name="onBox"
+        value="<?= $onBox ?>"
+    />
+    <small class="form-text text-muted">For example: 24</small>
+</div>
+<div class="form-group">
+    <label>Weight (kg)</label>
+    <input
+        class="form-control"
+        type="text"
+        name="weight"
+        value="<?= $weight ?>"
+    />
+    <small class="form-text text-muted">For example: 2.09</small>
+</div>
+<div class="form-group">
+    <label>M<sup>3</sup></label>
+    <input
+        class="form-control"
+        type="text"
+        name="m3"
+        value="<?= $m3 ?>"
+    />
+    <small class="form-text text-muted">For example: 0.0087</small>
+</div>
+<div class="form-group">
+    <label>Series</label>
+    <input
+        class="form-control"
+        type="text"
+        name="series"
+        value="<?= $series ?>"
+    />
+    <small class="form-text text-muted">For example: star series</small>
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
