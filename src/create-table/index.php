26e71027a987e84da157e2091e57e8a9679d318c
Pavel-Innokentevich-Galanin <PavelGalanin2001@outlook.com>
2020-12-28 03:16:18 +0300

    update module createTable

--- src/create-table/index.php
+++ src/create-table/index.php
@@ -2,58 +2,14 @@
     $title = "Create table";
     include '../_includesHTML/header.php';
     include '../_includesHTML/menu.php';
-    include '../_includesPHP/connect/connect.php';
+    
 ?>
 
 <div class="container">
 
     <?php
-        $sql = "CREATE TABLE `$table` (
-            `id` int NOT NULL,
-            `model` varchar(30) NOT NULL,
-            `name` varchar(200) NOT NULL,
-            `onBox` int NOT NULL,
-            `weight` float NOT NULL,
-            `m3` float NOT NULL,
-            `series` varchar(200) NOT NULL
-          ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"; //SQL запрос
-        $text = "Скрипт, который создает таблицу `$table`";
-        if ($connect->query($sql) === TRUE)
-        {
-            echo '<p class="text-success">' . $text . ', выполнен успешно!';
-        }
-        else
-        {
-            echo '<p class="text-danger">' . $text . ', не выполнен успешно:';
-            echo '<p>' . $connect->error;
-        }
-
-        $sql = "ALTER TABLE `$table`
-        ADD UNIQUE KEY `id` (`id`);"; //SQL запрос
-        $text = "Скрипт, который делает поле id уникальным";
-        if ($connect->query($sql) === TRUE)
-        {
-            echo '<p class="text-success">' . $text . ', выполнен успешно!';
-        }
-        else
-        {
-            echo '<p class="text-danger">' . $text . ', не выполнен успешно:';
-            echo '<p>' . $connect->error;
-        }
-
-        $sql = "ALTER TABLE `$table`
-        MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;"; //SQL запрос
-        $text = "Скрипт, который делает икремент поля id";
-        if ($connect->query($sql) === TRUE)
-        {
-            echo '<p class="text-success">' . $text . ', выполнен успешно!';
-        }
-        else
-        {
-            echo '<p class="text-danger">' . $text . ', не выполнен успешно:';
-            echo '<p>' . $connect->error;
-        }
-
+        include '../_includesPHP/connect/connect.php';
+        include '../_includesPHP/createTable/createTable.php';
         $connect->close(); // разорвали соединение
     ?>
 
